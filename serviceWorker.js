b5a10324bf576818b7c467277e1fb02a16e9e312
Glitch (patanjali-yoga-sutra) <none>
2025-05-26 16:23:26 +0000

    😼🎢 Updated with Glitch

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticDev = "yoga_sutra"
+const service_worker_version = "v2.0"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/patanjali_yoga_sutra.txt",
+  "/script.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
